56e419dd4e82c23f86e7aaadac0123c80c3c51ba
Deerclops <1638365039@qq.com>
2017-12-08 21:34:58 +0800

    Volley初始化类Rop新增session过期回调入口

--- EasyVolley/src/main/java/com/terrence/easyvolley/app/Rop.java
+++ EasyVolley/src/main/java/com/terrence/easyvolley/app/Rop.java
@@ -4,23 +4,27 @@ import android.content.Context;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.terrence.easyvolley.net.callback.ISessionExpired;
 
 /**
  * Created by DarkSouls on 2017/12/22.
  */
 
-public class Rop {
+public final class Rop {
 
     public static RequestQueue sRequestQueue;
     public static String sAppSecret;
     public static String sAppCode;
+    public static ISessionProvider sSessionProvider;
 
-    public static void init(Context context, String appSecret, String appCode) {
+    public static void init(Context context, String appSecret, String appCode, ISessionProvider provider) {
         sRequestQueue = Volley.newRequestQueue(context.getApplicationContext());
         sRequestQueue.start();
 
         sAppSecret = appSecret;
         sAppCode = appCode;
+
+        sSessionProvider = provider;
     }
 
     public static RequestQueue getRequestQueue() {
@@ -31,7 +35,19 @@ public class Rop {
         return sAppSecret;
     }
 
-    public static String getAppCode(){
+    public static String getAppCode() {
         return sAppCode;
+    }
+
+    public static String getSession() {
+        if (sSessionProvider != null) {
+            return sSessionProvider.getSession();
+        } else {
+            return "";
+        }
+    }
+
+    public interface ISessionProvider {
+        String getSession();
     }
 }
